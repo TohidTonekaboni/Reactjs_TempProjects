1ca0d78886c63388ac034d1745644f0ec5651b41
unknown <ttonkaboni@gmail.com>
2023-04-29 12:56:26 +0430

    Navbar component

--- restaurant/src/App.tsx
+++ restaurant/src/App.tsx
@@ -1,6 +1,17 @@
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./containers";
+import { Navbar } from "./components";
+
 import "./App.css";
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
 function App() {
   return (
     <div>
