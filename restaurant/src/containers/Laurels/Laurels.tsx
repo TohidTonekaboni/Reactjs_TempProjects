56729d31506d361f893f1835283a2b41a64a274d
unknown <ttonkaboni@gmail.com>
2023-04-30 09:55:12 +0430

    Last Commit

--- restaurant/src/containers/Laurels/Laurels.tsx
+++ restaurant/src/containers/Laurels/Laurels.tsx
@@ -1,8 +1,44 @@
-import './Laurels.css'
-const Laurels = () => {
-  return (
-    <div>Laurels</div>
-  )
+import "./Laurels.css";
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
+
+interface Award {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
 }
 
-export default Laurels
+const AwardCard = ({ award: { imgUrl, title, subtitle } }: { award: Award }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
+const Laurels = () => {
+  return (
+    <div className="app__bg app__wrapper section__padding" id="awards">
+      <div className="app__wrapper_info">
+        <SubHeading title="Awards & recognition" />
+        <h1 className="headtext__cormorant">Our Laurels</h1>
+
+        <div className="app__laurels_awards">
+          {data.awards.map((award) => (
+            <AwardCard award={award} key={award.title} />
+          ))}
+        </div>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.laurels} alt="laurels_img" />
+      </div>
+    </div>
+  );
+};
+
+export default Laurels;
